1a9a377b051f70cd71bc8664e9a99b5bc369dda4
Oikawa <knasyrova622@gmail.com>
2022-08-16 05:57:55 -0700

    1dz

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About from "./components/About/AboutUs";
+import Portfolio from "./components/Portfolio/Portfolio";
+function App() {
+  return (
+  <>
+    <About/>
+    <Portfolio/>
+  </>
+  )
+}
+export default App;
---
+++ src/components/About/AboutUs.js
@@ -0,0 +1,10 @@
+function About(){
+  return(
+      <>
+      <h1>title</h1>
+       <p>text text</p>
+       </>
+  )
+}
+
+export default About
