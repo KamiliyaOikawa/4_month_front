b27d7522504378c8331229084b8662854f58ae2e
Oikawa <knasyrova622@gmail.com>
2022-08-20 03:13:13 -0700

    2dz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import About from "./components/About/AboutUs";
 import Portfolio from "./components/Portfolio/Portfolio";
+import AboutPage from "./pages/aboutPage/AboutPage";
+
 function App() {
+  const info = {
+    title: "I am title",
+    body: "Lorem inpsum bla bla bla"
+  }
   return (
-  <>
-    <About/>
-    <Portfolio/>
-  </>
+      <>
+        <About/>
+        <Portfolio/>
+        <AboutPage infop={info} />
+      </>
   )
 }
+
 export default App;
+ 
---
+++ src/pages/aboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+function AboutPage({infop}){
+  return(
+      <>
+      <h1>{infop.title}</h1>
+        <p >{infop.body}</p>
+      </>
+  )
+}
+
+export default AboutPage
+
